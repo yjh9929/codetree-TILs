65592a95d3d351edcff2d715dfdcf0c3c58018e3
Yoon Jihee <74297999+yjh9929@users.noreply.github.com>
2024-04-07 21:42:31 +0900

    Title: 바이러스 검사, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240407/바이러스 검사/virus-detector.py
+++ 240407/바이러스 검사/virus-detector.py
@@ -2,15 +2,16 @@ n= int(input())
 
 restaurant = list(map(int,input().split()))
 
-headT, memT = map(int,input().split())
+headTmax, memTmax = map(int,input().split())
 
 minT = n
 min_memT = 0
 for i in range(len(restaurant)):
-    cnt = 0
-    restaurant[i] -= headT
-    if restaurant[i] > 0:
-        min_memT = restaurant[i] // memT
-        min_memT += 1
-    minT += min_memT
+    restaurant[i] -= headTmax
+    if restaurant[i] == 0:
+        continue
+    else:
+        minT += restaurant[i] // memTmax
+        if restaurant[i] % memTmax != 0:
+            minT += 1
 print(minT)
