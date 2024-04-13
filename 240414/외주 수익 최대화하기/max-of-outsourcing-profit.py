cdbaea1c83004775989df05b29139f01b6931c2b
Yoon Jihee <74297999+yjh9929@users.noreply.github.com>
2024-04-14 00:26:22 +0900

    Title: 외주 수익 최대화하기, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240414/외주 수익 최대화하기/max-of-outsourcing-profit.py
+++ 240414/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -1,20 +1,14 @@
 N = int(input())
-T = [0] * N
-P = [0] * N
+T = [0]*N
+P = [0]*N
+for i in range(N):
+    T[i], P[i] = map(int, input().split())
 
-def dfs(n, sm):
-	global ans
-	# [1] 종료 조건 : 가능한 n을 종료에 관련된 것으로 정의
-	if n >= N:
-		ans = max(ans, sm)
-		return
-	# [2] 하부 함수 호출 : 화살표 개수만큼 호출 
-	if n + T[n] <= N: # 외주를 받는 경우 (휴가 끝나기 전까지만 가능)
-		dfs(n + T[n], sm + P[n])
-	dfs (n + 1, sm) # 외주를 받지 않은 경우 (항상 바로 다음날로 가능)
-for i in range(N): 
-	T[i], P[i] = map(int, input().split())
+dp = [0]*(N+1)
+for n in range(N-1, -1, -1):    # 뒤에서 앞으로(완료기준)
+    if n+T[n]<=N:               # 기간내 상담완료 가능
+        dp[n]=max(dp[n+1], dp[n+T[n]]+P[n])
+    else:
+        dp[n]=dp[n+1]
 
-ans = 0
-dfs(0, 0)
-print(ans)
+print(dp[0])
